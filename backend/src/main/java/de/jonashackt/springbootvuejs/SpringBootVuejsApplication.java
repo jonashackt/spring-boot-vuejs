3123b973d6615ac6c0dc262b0db61238f3d543d0
Jonas Hecht <jonas.hecht@codecentric.de>
2017-09-11 14:10:40 +0200

    Initial commit with module pom.xmls and initialized Vue.js frontend project (vue init webpack frontend) and initialized Spring Boot backend (via Spring Initializr).

---
+++ backend/src/main/java/de/jonashackt/springbootvuejs/SpringBootVuejsApplication.java
@@ -0,0 +1,12 @@
+package de.jonashackt.springbootvuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVuejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVuejsApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/de/jonashackt/springbootvuejs/SpringBootVuejsApplicationTests.java
@@ -0,0 +1,16 @@
+package de.jonashackt.springbootvuejs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootVuejsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

49443305d506e0ea939058c183cbcae479cdd544
Jonas Hecht <jonas.hecht@codecentric.de>
2018-04-09 16:16:34 +0200

    #11: Adding access to PORT in test.env.js

--- frontend/config/test.env.js
+++ frontend/config/test.env.js
@@ -2,5 +2,6 @@ var merge = require('webpack-merge')
 var devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  API_PORT: JSON.stringify(process.env.PORT)
 })

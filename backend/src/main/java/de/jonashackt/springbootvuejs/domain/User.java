41a040da48cd1b698bb778c3b10e96043565c643
Jonas Hecht <jonas.hecht@codecentric.de>
2018-04-23 10:12:29 +0200

    Wow, only PostgreSQL doesn´t support the usage of a 'user' table (see https://stackoverflow.com/a/49573003/4964553 and https://stackoverflow.com/questions/22256124/cannot-create-a-database-table-named-user-in-postgresql). So we have to change the table name to something else, here 'users'

--- backend/src/main/java/de/jonashackt/springbootvuejs/domain/User.java
+++ backend/src/main/java/de/jonashackt/springbootvuejs/domain/User.java
@@ -1,11 +1,9 @@
 package de.jonashackt.springbootvuejs.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="users")
 public class User {
 
     // PrimaryKey

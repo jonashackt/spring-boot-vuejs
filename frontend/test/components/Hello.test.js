3e72f01bf15d8eac77dd2b352a5fe839e5a81e7f
Jonas Hecht <jonas.hecht@codecentric.de>
2018-06-27 22:34:51 +0200

    Now Jest testing is done after the npm build run - just as the Maven build. Also added some given - when - then for visibility.

--- frontend/test/components/Hello.test.js
+++ frontend/test/components/Hello.test.js
@@ -3,8 +3,11 @@ import Hello from '@/components/Hello'
 
 describe('Hello.vue', () => {
   it('should render correct hello message', () => {
-    const wrapper = shallowMount(Hello, { propsData: { hellomsg: 'Welcome to your Jest powered Vue.js App' } });
-    const contentH1 = wrapper.find('h1');
+    // Given
+    const hellowrapped = shallowMount(Hello, { propsData: { hellomsg: 'Welcome to your Jest powered Vue.js App' } });
+    // When
+    const contentH1 = hellowrapped.find('h1');
+    // Then
     expect(contentH1.text()).toEqual('Welcome to your Jest powered Vue.js App');
   })
 })

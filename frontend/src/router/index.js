88a6ece054b755675afa888ae552ef489d2fbb4f
Jonas Hecht <jonas.hecht@codecentric.de>
2018-06-28 08:18:42 +0200

    Fixing VueRouter definition

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Service from '@/components/Service'
 import Bootstrap from '@/components/Bootstrap'
 import User from '@/components/User'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
 export default new Router({
   routes: [

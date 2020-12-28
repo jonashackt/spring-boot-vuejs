3787b66fd9268c97284dfbc12d25e53c34dd4d0e
Micah Silverman <micah@afitnerd.com>
2020-12-28 15:49:31 -0500

    Fixed forwarding from spring boot -> vuejs.

---
+++ backend/src/main/java/de/jonashackt/springbootvuejs/controller/VueForwardController.java
@@ -0,0 +1,21 @@
+package de.jonashackt.springbootvuejs.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VueForwardController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(VueForwardController.class);
+
+    // Forwards all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
+    // Required because of 'mode: history' usage in frontend routing, see README for further details
+    @RequestMapping(value = "{_:^(?!index\\.html|api).*$}")
+    public String redirectApi() {
+        LOG.info("URL entered directly into the Browser, so we need to redirect...");
+        return "forward:/";
+    }
+
+}
